ba378611a9bf04112dd899d69d5601eb5b1aec7f
superkebab <felgines.elliot@gmail.com>
2015-10-20 22:15:18 +0200

    Routes

--- wr-share.js
+++ wr-share.js
@@ -1,19 +1,73 @@
 Songs = new Mongo.Collection("songs");
+Radios = new Mongo.Collection("radios");
 
-if (Meteor.isClient) { 
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
+Router.route('/', {
+    name: 'home',
+    template: 'home'
+});
+
+Router.route('/r/:name', {
+    name: 'radio',
+    template: 'radio',
+    data: function(){
+        var radioName = this.params.name;
+        return Radios.findOne({ name: radioName });
+    }
+});
+
+if (Meteor.isClient) {
     Session.setDefault("volume", 50);
-
-    Template.body.helpers({
-        songs: function () {
-            return Songs.find({}, {sort: {createdAt: 1}});
-        }
-    });
 
     Template.registerHelper('volume',function(input){
         return Session.get("volume");
     });
 
-    Template.body.events({
+    Template.home.events({
+        "submit .join-radio": function (event) {
+            event.preventDefault();
+
+            // Get value from form element
+            var name = event.target.name.value;
+
+            // If the radio doesn't exist
+            if (Radios.find({ name: name }).count() == 0) {
+                // Insert a radio into the collection
+                Radios.insert({
+                    name: name,
+                    createdAt: new Date() // current time
+                });
+            }
+
+            // Clear form
+            event.target.name.value = "";
+
+            // Go to the radio
+            Router.go('radio', {name: name});
+        }
+    });
+
+    Template.home.helpers({
+        radios: function () {
+            return Radios.find({}, {});
+        }
+    });
+    
+    Template.radio.helpers({
+        currentSongs: function () {
+            var radio = Radios.findOne({ name: this.params.name });
+            return Songs.find({}, {sort: {createdAt: 1}, limit : 1});
+        },
+        songs: function () {
+            var radio = Radios.findOne({ name: this.params.name });
+            return Songs.find({}, {sort: {createdAt: 1}, skip: 1});
+        }
+    });
+    
+    Template.radio.events({
         "submit .new-song": function (event) {
             // Prevent default browser form submit
             event.preventDefault();
@@ -38,18 +92,18 @@ if (Meteor.isClient) {
             event.target.title.value = "";
             event.target.link.value = "";
         },
-
+        
         "click .volume-up": function (event) {
             if (Session.get("volume") < 100)
                 Session.set("volume", Session.get("volume")+1);
         },
-
+        
         "click .volume-down": function (event) {
             if (Session.get("volume") > 0)
                 Session.set("volume", Session.get("volume")-1);
         }
     });
-
+    
     Template.song.events({
         "click .delete": function () {
             Songs.remove(this._id);
@@ -57,8 +111,8 @@ if (Meteor.isClient) {
     });
 }
 
-if (Meteor.isServer) { 
-    Meteor.startup(function () { 
-        // code to run on server at startup 
-    });
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
 }
